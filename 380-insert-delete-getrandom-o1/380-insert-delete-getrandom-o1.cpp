276dd49cead89f316a68fbec77e3efafb7b427d1
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-03-28 06:59:51 +0530

    Time: 371 ms (32.80%), Space: 96.9 MB (84.53%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
@@ -1,39 +1,33 @@
 class RandomizedSet {
 public:
-    RandomizedSet() : seed((random_device())()) {}
-
+    RandomizedSet() {
+        
+    }
+    unordered_set<int>st;
     bool insert(int val) {
-        if (lookup.count(val)) return false;
-        lookup[val] = values.size();
-        values.emplace_back(val);
+        if(st.count(val)) return false;
+        else
+            st.insert(val);
         return true;
     }
     
     bool remove(int val) {
-        if (!lookup.count(val)) return false;
-        
-        // copy the last value in the array
-        // to the current location of the value
-        // we want to remove
-        values[lookup[val]] = values.back();
-        
-        // set the location of the last value
-        // in the array to the current location
-        lookup[values.back()] = lookup[val];
-        
-        // remove the current element
-        values.pop_back();
-        lookup.erase(val);
-
-        return true;
+        if(st.count(val)){
+            st.erase(val);
+            return true;}
+        else
+            return false;
     }
     
     int getRandom() {
-        return values[uniform_int_distribution<>
-                        (0, values.size() - 1)(seed)];
+      return *next(st.begin(),rand()%st.size());
     }
-private:
-    unordered_map<int, int> lookup;
-    vector<int> values;
-    default_random_engine seed;
 };
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
