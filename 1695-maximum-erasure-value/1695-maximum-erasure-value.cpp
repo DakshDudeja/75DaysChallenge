f680c7909127c01a9602c345bb13b9a57a120cb7
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-06-12 22:26:00 +0530

    Time: 237 ms (94.17%), Space: 89.2 MB (99.27%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+int maximumUniqueSubarray(vector<int>& nums) {        
+	int n = size(nums), cur_sum = 0, ans = 0, l = 0, r = 0;
+    bool freq[10001]{false};
+	while(r < n) {
+		while(freq[nums[r]])
+			cur_sum -= nums[l], freq[nums[l++]] = false;
+		cur_sum += nums[r], freq[nums[r++]] = true;  
+		ans = max(ans, cur_sum);    
+	}
+	return ans;
+}
+
+};
