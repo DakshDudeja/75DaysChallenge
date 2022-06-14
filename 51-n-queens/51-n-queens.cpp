0f755b4120efe51b5f45c0c6fbf8a8e69956c71f
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-06-14 16:44:08 +0530

    Time: 10 ms (57.75%), Space: 7.4 MB (58.99%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,37 +1,48 @@
 class Solution {
 public:
-        vector<vector<string>> ans;
-
-     bool isValid(vector<string> &board, int row, int col){
-        // check col
-        for(int i=row;i>=0;--i)
-            if(board[i][col] == 'Q') return false;
-        // check left diagonal
-        for(int i=row,j=col;i>=0&&j>=0;--i,--j)
-            if(board[i][j] == 'Q') return false;
-        //check right diagonal
-        for(int i=row,j=col;i>=0&&j<board.size();--i,++j)
-            if(board[i][j] == 'Q') return false;
+    vector<vector<string>>res;
+    bool isSafe(int m ,int n,vector<string>&grid){
+        //column
+        for(int i=m,j=n;i>=0;i--){
+            if(grid[i][j]=='Q') return false;
+        }
+        //left diagonal
+        for(int i=m,j=n;i>=0 and j>=0;i--,j--){
+            if(grid[i][j]=='Q') return false;
+        }
+        //right diagonal
+        for(int i=m,j=n;i>=0 and j<grid[i].size();i--,j++){
+            if(grid[i][j]=='Q') return false;
+        }
         return true;
     }
-    void solve(int n,int row,vector<string>&board){
-        if(row==board.size()){
-            ans.push_back(board);
+    void solve(int i,vector<string>&temp,vector<string>&grid,int n){
+        if(i>=n){
+            res.push_back(grid);
             return;
         }
-        if(row<0 ) return;
-        for(int col =0;col<board.size();col++){
-            if(isValid(board,row,col)){
-                board[row][col] = 'Q';
-                solve(n,row+1,board);
-                board[row][col] = '.';
+        for(int k=0;k<n;k++){
+            if(isSafe(i,k,grid)){
+                grid[i][k]='Q';
+                solve(i+1,temp,grid,n);
+                grid[i][k]='.';
             }
         }
+        return;
     }
     vector<vector<string>> solveNQueens(int n) {
-         if(n <= 0) return {{}};
-        vector<string> board(n,string(n,'.'));
-        solve(n,0,board);
-        return ans;
+        // vector<vector<string>>grid(n,vector<string>(n,"."));
+        vector<string> grid(n,string(n,'.'));   //1d ke andar 2d vector bana diya 
+
+        vector<string>temp;
+        solve(0,temp,grid,n);
+        return res;
+        
     }
 };
+
+//grid iterate 
+//issafe
+//mark true
+//iterate full
+//mark false
