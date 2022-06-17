259b32fb9bd53b3054c2199ef7475af81dddb96e
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-06-17 08:33:53 +0530

    Time: 51 ms (65.51%), Space: 89.4 MB (62.19%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    double mediann(vector<int>&a,vector<int>&b){
-        int m=a.size();
-        int n=b.size();
-        if(m>n)
-            return mediann(b,a);
-        int l=0,r=m;
-        while(l<=r){
-            int partx=l+(r-l)/2;
-            int party=(m+n+1)/2-partx;
-            int maxlx=(partx==0)?INT_MIN:a[partx-1];
-            int minrx=(partx==m)?INT_MAX:a[partx];
-            int maxly=(party==0)?INT_MIN:b[party-1];
-            int minry=(party==n)?INT_MAX:b[party];
-            if(maxlx<=minry&&maxly<=minrx){
-                if((m+n)%2==0)
-                    return (double)(max(maxlx,maxly)+min(minrx,minry))/2;
-                else
-                    return (double)(max(maxlx,maxly));
-            }else if(maxlx>minry)
-                r=partx-1;
-            else
-                l=partx+1;
-        }
-        return -1.0;
-    }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double ans;
-        ans=mediann(nums1,nums2);
-        return ans;   
+        if(nums2.size()<nums1.size()) return findMedianSortedArrays(nums2,nums1);
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int low=0,high=n1;
+        while(low <= high){
+            int cut1= (low+high)/2;
+            int cut2=(n1+n2+1)/2-cut1;
+            
+            int left1 = cut1 ==0 ?INT_MIN: nums1[cut1-1];
+            int left2 = cut2 ==0 ?INT_MIN : nums2[cut2-1];
+            
+            int right1 = cut1 == n1 ?INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ?INT_MAX : nums2[cut2];
+            
+            if(left1 <= right2 and left2 <= right1){
+                if((n1+n2) % 2==0)
+                    return (max(left1,left2) + min(right1,right2)) / 2.0;
+                else
+                    return max(left1,left2);
+            }
+            else if(left1 > right2){
+                high=cut1-1;
+            }
+            else{
+                low=cut1+1;
+            }
+        }
+        return 0.0;
     }
 };
