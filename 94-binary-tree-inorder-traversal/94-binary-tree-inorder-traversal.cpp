b092609ea30039f7ce1457a1d95c8b265ae571f0
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-06-23 14:09:46 +0530

    Time: 4 ms (40.22%), Space: 8.3 MB (91.65%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,38 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void addLeftSubTree(TreeNode *temp,stack<TreeNode*>&st){
-        st.push(temp);
-        while(temp->left!=NULL){
-            st.push(temp->left);
-            temp=temp->left;
-        }
+    vector<int>res;
+    void solve(TreeNode* root){
+        if(root==NULL)return;
+        
+        solve(root->left);
+        res.push_back(root->val);
+        solve(root->right);
         return;
     }
     vector<int> inorderTraversal(TreeNode* root) {
-         vector<int>res;
-        stack<TreeNode*>st;
-        if(root==NULL) return res;
-        addLeftSubTree(root,st);
-
-        while(!st.empty()){
-            TreeNode* temp = st.top();
-            st.pop();
-            res.push_back(temp->val);
-            
-            if(temp->right)
-                addLeftSubTree(temp->right,st);            
-        }
+        solve(root);
         return res;
     }
 };
