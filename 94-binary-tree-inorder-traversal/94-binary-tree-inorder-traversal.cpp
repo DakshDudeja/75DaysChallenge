d3b53787f62703660ccf8382e7fcd3c32aa447eb
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-06-23 14:35:19 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (16.17%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    vector<int>res;
-    void solve(TreeNode* root){
-        if(root==NULL)return;
-        
-        solve(root->left);
-        res.push_back(root->val);
-        solve(root->right);
+    void addLeftSubTree(TreeNode *temp,stack<TreeNode*>&st){
+        st.push(temp);
+        while(temp->left!=NULL){
+            st.push(temp->left);
+            temp=temp->left;
+        }
         return;
     }
     vector<int> inorderTraversal(TreeNode* root) {
-        solve(root);
+         vector<int>res;
+        stack<TreeNode*>st;
+        if(root==NULL) return res;
+        addLeftSubTree(root,st);
+
+        while(!st.empty()){
+            TreeNode* temp = st.top();
+            st.pop();
+            res.push_back(temp->val);
+            
+            if(temp->right)
+                addLeftSubTree(temp->right,st);            
+        }
         return res;
     }
 };
