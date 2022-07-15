2debc65dcd4f537868221962b6c027e5afaab4bd
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-07-15 08:21:40 +0530

    Time: 113 ms (5.02%), Space: 44.2 MB (5.06%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-    vector<int>par,size;
-    int findPar(int u){
-        return par[u] == u ? u : (par[u] = findPar(par[u]));
-    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n= grid.size();
-        if(n==0) return 0;
-        int m= grid[0].size();
-        
-        for(int i=0;i<n*m;i++){
-            par.push_back(i);
-            size.push_back(1);
-        }
-        
-        vector<vector<int>> dir {{0,1},{0,-1},{1,0},{-1,0}};
-        int maxSize=0;
-        
+        int n=grid.size(),m=grid[0].size();
+        queue<pair<int,int>>q;
+        int cnt=0,ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 1){
-                    int p1 = findPar(i*m+j);
-                    for(vector<int>&d: dir){
-                        int r = i+d[0];
-                        int c = j+d[1];
+                if(grid[i][j]==1){
+                    q.push({i,j});
+                    int curArea = 0;
+                    grid[i][j]=0;
+                    
+                    while(!q.empty()){
+                        int currR = q.front().first;
+                        int currC= q.front().second;
+                        q.pop();
                         
-                        if(r>=0 && c>=0 and r<n and c<m and grid[r][c]==1){
-                            int p2= findPar(r*m+c);
-                            if(p1!=p2){
-                                par[p2] = p1;
-                                size[p1] += size[p2];
+                        curArea++;
+                        
+                        vector<vector<int>>dir={{0,1},{1,0},{0,-1},{-1,0}};
+                        for(auto it:dir){
+                            int row = it[0]+currR;
+                            int col = it[1]+currC;
+
+                            if(row < 0 || col <0 || row>=n || col >=m || grid[row][col]==0) continue;
+
+                            if(grid[row][col]==1) {
+                                cnt++;
+                                q.push({row,col});
+                                grid[row][col]=0;
                             }
                         }
                     }
-                    maxSize = max(maxSize,size[p1]);
+                  
+                    if(curArea > ans) ans=curArea;
                 }
             }
         }
-        return maxSize;
+        return ans;            
     }
 };
