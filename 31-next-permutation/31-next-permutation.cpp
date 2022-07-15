9840a6cdf1be0627a021f88becf33f1b0a05f736
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-07-15 09:15:51 +0530

    Time: 4 ms (76.11%), Space: 12.2 MB (36.60%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int bp=-1;
-        int n=nums.size();
+        int n=nums.size();   //algorithm to find next permutation lexicographically
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 bp=i;
-                break;
-                
+                break;   
             }
-            
         }
         if(bp<0){
             reverse(nums.begin(),nums.end());
@@ -21,11 +19,8 @@ public:
                 bp2=i;
                 break;
             }
-            
         }
         swap(nums[bp],nums[bp2]);
         reverse(nums.begin() + bp + 1, nums.end());
-
-        
     }
 };
