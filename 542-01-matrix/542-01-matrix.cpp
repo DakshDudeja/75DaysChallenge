b6487fea8bf53da9663719e468410eab3ff84b0b
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-07-18 08:40:36 +0530

    Time: 355 ms (7.64%), Space: 70.3 MB (5.15%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -5,38 +5,29 @@ public:
         if(m==0 || n==0) return mat;
         
         queue<pair<int,int>>q;
-        vector<vector<bool>>vis(m,vector<bool>(n,false));
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j]==0){
+                if(mat[i][j]==0)
                     q.push({i,j});
-                    vis[i][j]=true;
-                }
+                else
+                    mat[i][j]=-1;
             }
         }
         vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
         while(!q.empty()){
-            int size=q.size();
-            while(size-- >0){
-                int currR = q.front().first;
-                int currC= q.front().second;
-                q.pop();
-
-                for(auto it:dir){
-                    int i = currR + it[0];
-                    int j = currC + it[1];
-
-                    if(i>=0 and j>=0 and i<m and j<n and vis[i][j]==false){
-
-                        q.push({i,j});
-                        mat[i][j]=mat[currR][currC]+1;
-                        vis[i][j]=true;
-                    }
+            int currR = q.front().first;
+            int currC= q.front().second;
+            q.pop();
+            
+            for(auto it:dir){
+                int i = currR + it[0];
+                int j = currC + it[1];
+                
+                if(i>=0 and j>=0 and i<m and j<n and mat[i][j]<0){
+                    q.push({i,j});
+                    mat[i][j]=mat[currR][currC]+1;
                 }
             }
-            
-            
         }
         return mat;
     }
