be8ee5a217f8efc7aa894eabb7c4472a32398119
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-07-21 14:14:27 +0530

    Time: 669 ms (5.00%), Space: 164.5 MB (5.42%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    int solve(int i,int canBuy,int transCount,vector<int>&prices,vector<vector<int>>&dp){
-        if(i>=prices.size() or transCount <= 0)return 0;
+    int solve(int i,int canBuy,int k,vector<int>& prices,vector<vector<int>>&dp){
+        if(i>=prices.size() || k<=0) return 0;
+        
         if(dp[i][canBuy]!=-1) return dp[i][canBuy];
+        
+        int idle = solve(i+1,canBuy,k,prices,dp);
         int buy=0,sell=0;
-        int notConsider=solve(i+1,canBuy,transCount,prices,dp);
-
-        if(canBuy)
-            buy= -prices[i] + solve(i+1,0,transCount,prices,dp);
+        if(canBuy){
+             buy = -prices[i] + solve(i+1,0,k,prices,dp);
+        }
         else
-            sell= prices[i] +solve(i+1,1,transCount-1,prices,dp);
-
-        return dp[i][canBuy]= max(notConsider,max(buy,sell));
+          sell = +prices[i] + solve(i+1,1,k-1,prices,dp);
+        
+        return dp[i][canBuy] = max(idle,max(buy,sell));
         
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
         return solve(0,1,1,prices,dp);
+
     }
 };
