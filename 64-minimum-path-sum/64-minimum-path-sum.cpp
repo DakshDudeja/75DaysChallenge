e8df896b70cb0cca854c760dc85b58b9848b5d82
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-07-27 16:55:00 +0530

    Time: 23 ms (15.22%), Space: 9.8 MB (68.95%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     int dp[201][201];
     int solve(int i,int j,vector<vector<int>>&grid){
-        // if(i>=grid.size() || j>=grid[0].size()) return INT_MAX;
-        
+        if(i>=grid.size() || j>=grid[0].size()) return 1000;
         if(i==grid.size()-1 and j==grid[0].size()-1) return grid[i][j];
         
         if(dp[i][j]!=-1) return dp[i][j];
         
-        int a=INT_MAX,b=INT_MAX;
-         if(i+1<grid.size()) a= grid[i][j] + solve(i+1,j,grid);
-         
-         if(j+1<grid[0].size()) b= grid[i][j] + solve(i,j+1,grid);
-
+         int a= grid[i][j] + solve(i+1,j,grid);         
+         int b= grid[i][j] + solve(i,j+1,grid);
         
         return dp[i][j]=min(a,b);
     }
