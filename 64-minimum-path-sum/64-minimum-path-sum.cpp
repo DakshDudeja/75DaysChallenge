aa5352446078c4e76bb86e5c68d122eb49ea902c
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-07-27 16:50:58 +0530

    Time: 22 ms (18.31%), Space: 10 MB (61.60%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int sum=0;
     int dp[201][201];
-    int solve(int i,int j,vector<vector<int>>& grid){
-        if(i>=grid.size() || j>=grid[0].size()) return 1000;
-        if(i==grid.size()-1 and j==grid[0].size()-1){
-            return grid[i][j];
-        }
+    int solve(int i,int j,vector<vector<int>>&grid){
+        // if(i>=grid.size() || j>=grid[0].size()) return INT_MAX;
+        
+        if(i==grid.size()-1 and j==grid[0].size()-1) return grid[i][j];
+        
         if(dp[i][j]!=-1) return dp[i][j];
-        int a = grid[i][j] + solve(i+1,j,grid);
-        int b = grid[i][j] + solve(i,j+1,grid);
+        
+        int a=INT_MAX,b=INT_MAX;
+         if(i+1<grid.size()) a= grid[i][j] + solve(i+1,j,grid);
+         
+         if(j+1<grid[0].size()) b= grid[i][j] + solve(i,j+1,grid);
+
         
         return dp[i][j]=min(a,b);
-
     }
     int minPathSum(vector<vector<int>>& grid) {
-        memset(dp,(-1),sizeof(dp));
+        
+        memset(dp,-1,sizeof(dp));
         return solve(0,0,grid);
     }
 };
