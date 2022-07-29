a999dce6487a69a0318972ee433c2a4ff18831d3
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-07-29 09:18:32 +0530

    Time: 665 ms (22.76%), Space: 51.7 MB (18.65%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,37 +1,41 @@
-#define pii pair<int, pair<int,int>>
-
 class Solution {
 public:
-    //Directions (top, right, bottom, left)
-    const int d4x[4] = {-1,0,1,0}, d4y[4] = {0,1,0,-1};
-    
     int minimumEffortPath(vector<vector<int>>& h) {
-        int n = h.size(), m = h[0].size();
-        //min-heap
-        priority_queue <pii, vector<pii>, greater<pii>> pq;
-        //to store distances from (0,0)
-        vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
-        dis[0][0] = 0;
-        pq.push({0, {0, 0}});
         
-        //Dijstra algorithm
-        while(!pq.empty()) {
-            pii curr = pq.top(); pq.pop();
-            int d = curr.first, r = curr.second.first, c = curr.second.second;
-            // bottom right position
-            if(r==n-1 && c==m-1) return d;
-            for(int i=0; i<4; ++i) {
-                int nx = r+d4x[i], ny = c+d4y[i];
-                //check if new position is invalid
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;
-                int nd = max(d, abs(h[nx][ny] - h[r][c]));
-                if (nd < dis[nx][ny]) {
-                    dis[nx][ny] = nd;
-                    pq.push({nd, {nx, ny}});
+        int n=h.size(),m=h[0].size();
+        priority_queue<vector<int>>pq;
+        pq.push({0,0,0});
+        
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
+        dist[0][0]=0;
+        
+        vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
+        while(pq.size()){
+            auto currNode = pq.top();
+            pq.pop();
+            
+            int currWt = -currNode[0];
+            int currR =  currNode[1];
+            int currC =  currNode[2];
+            
+            if(currR==n-1 and currC==m-1) return currWt;
+            
+            for(auto it:dir){
+                int i=currR + it[0];
+                int j=currC + it[1];
+                
+                if(i>=0 and i<n and j>=0 and j<m ){
+                    int neuWt = max(currWt, abs(h[currR][currC] - h[i][j]));
+                    if(dist[i][j]>neuWt){
+                        dist[i][j]=neuWt;
+                        pq.push({-neuWt,i,j});
+
+                    }
+                    
                 }
             }
+            
         }
-        return 0;
-        //please upvote
+        return -1;
     }
 };
