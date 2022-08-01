79cb2ab87a98a1b1d4ff55062c2a427b7a32abaa
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-08-01 12:42:16 +0530

    Time: 180 ms (93.12%), Space: 101.2 MB (97.39%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -1,29 +1,32 @@
+
 class Solution {
-    struct S {
-        bool isBST;
-        int minVal; 
-        int maxVal; 
+public:
+    struct Node{
+        bool isBst;
+        int minVal;
+        int maxVal;
         int sum;
     };
     
-    S helper(TreeNode *node, int &res) {
-        if (!node) {
-            return { true, INT_MAX, INT_MIN, 0 };
+    Node solve(TreeNode* root,int &res){
+        if(!root) return{true,INT_MAX,INT_MIN,0} ;
+            
+        Node left = solve(root->left,res);
+        Node right = solve(root->right,res);
+        
+        if(left.isBst==true && right.isBst==true and left.maxVal <root->val and right.minVal > root->val){
+            int sum=left.sum+right.sum+root->val;
+            res=max(res,sum);
+            return {true,min(left.minVal,root->val) ,max(right.maxVal,root->val),sum};
         }
-        S l = helper(node->left, res);
-        S r = helper(node->right, res);
-        if (l.isBST && r.isBST && l.maxVal < node->val && r.minVal > node->val) {
-            int sum = l.sum + r.sum + node->val;
-            res = max(res, sum);
-            return { true, min(l.minVal, node->val), max(r.maxVal, node->val), sum };
-        } else {
-            return { false, INT_MIN, INT_MAX, 0 };
+        else{
+            return {false,INT_MIN,INT_MAX,0};
         }
     }
-public:
+    
     int maxSumBST(TreeNode* root) {
-        int res = 0;
-        helper(root, res);
+        int res=0;
+        solve(root,res);
         return res;
     }
 };
