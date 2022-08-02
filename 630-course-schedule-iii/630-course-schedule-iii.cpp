8b807c7b0f1896e528064d21fd4f067fce811fe4
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-08-02 16:29:33 +0530

    Time: 258 ms (95.48%), Space: 56.3 MB (64.18%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    static bool comp(const vector<int>&a,const vector<int>&b){
+    bool static cmp(vector<int>&a, vector<int>&b){
         return a[1]<b[1];
     }
-    int scheduleCourse(vector<vector<int>>& c) {
-        sort(c.begin(),c.end(),comp);
-        
-        int total_duration=0;
-        priority_queue<int>pq;
-        for(int i=0;i<c.size();i++){
-            
-            int curr_duration= c[i][0];
-            int deadline= c[i][1];
-            
-            total_duration +=curr_duration;
-            
-           pq.push(curr_duration);
-            
-            if(total_duration > deadline){
-                // we want to skip this course or
-                //try to skip the course which is taken and has maximum duration;
-                
-                total_duration-=pq.top();
+
+    int scheduleCourse(vector<vector<int>>& courses) {
+
+        int n=courses.size();
+        sort(courses.begin(), courses.end(), cmp);  //sort the given array on the basis of lastDay i.e. courses[i][1];
+
+        priority_queue<int> pq;  //build a max heap
+        int sum=0;
+
+        for(int i=0;i<n;i++){
+            pq.push(courses[i][0]);  //push duration of course in max heap and add it to the sum
+
+            sum+=courses[i][0];
+
+            if(sum>courses[i][1]){  //if at any instance, sum becomes greater than last day for a particula course, then pop and subtract it's duration from the sum
+                sum=sum-pq.top();
                 pq.pop();
             }
         }
-        return pq.size();
+        return pq.size();  //return pq.size
     }
 };
