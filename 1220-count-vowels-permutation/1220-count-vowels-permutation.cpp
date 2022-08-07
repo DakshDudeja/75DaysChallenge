a9e291a03e99eeca911f03c0deb86e6701a96ece
Daksh Dudeja <61489137+DakshDudeja@users.noreply.github.com>
2022-08-07 22:47:26 +0530

    Time: 203 ms (21.49%), Space: 25.3 MB (35.98%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    const int mod = 1e9 + 7;
+    int dp[26][100000];
+    unordered_map<char,vector<char>> mp;
+    int solve(char prev,int n){
+        if(!n) return 1;
+        if(dp[prev-'a'][n] != -1) return dp[prev-'a'][n];
+        
+        int ans = 0;
+        for(auto ch : mp[prev]) 
+            ans = (ans + solve(ch,n-1)) % mod;
+        
+        return dp[prev-'a'][n] = ans;
+    }
+public:
+    int countVowelPermutation(int n) {
+        mp.insert({'a',{'e'}});
+        mp.insert({'e',{'a','i'}});
+        mp.insert({'i',{'a','e','o','u'}});
+        mp.insert({'o',{'i','u'}});
+        mp.insert({'u',{'a'}});
+        mp.insert({'z',{'a','e','i','o','u'}});
+        
+        memset(dp,-1,sizeof dp);
+        
+        return solve('z',n);
+    }
+};
